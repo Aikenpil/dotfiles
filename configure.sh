ce46ff7387e4f3ea360faf4e79446dcccf8b7ace
aikenpil <78743601+Aikenpil@users.noreply.github.com>
2025-08-31 20:29:50 -0300

    adding a script to config a arch linux distro for me

---
+++ configure.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Configuration script for a new Arch Linux installation
+# Run this script as a regular user after the base system is installed.
+
+#init keyring
+sudo pacman-key --init
+sudo pacman-key --populate
+sudo pacman -S archlinux-keyring
+
+# Define the file path
+PACMAN_CONF="/etc/pacman.conf"
+
+# Check if the line already exists
+if ! grep -q "ParallelDownloads" "$PACMAN_CONF"; then
+	# Add ParallelDownloads = 5 under the [options] section
+	sudo sed -i '/^# Misc/a ParallelDownloads = 5' "$PACMAN_CONF"
+	echo "Added 'ParallelDownloads = 5' to $PACMAN_CONF"
+else
+	echo "ParallelDownloads setting already exists in $PACMAN_CONF"
+fi
+
+# --- 1. System Update ---
+echo "Updating the system..."
+sudo pacman -Syyuu --noconfirm
+echo "System updated."
+
+# --- 2. Install Your Favorite Packages ---
+echo "Installing core packages..."
+sudo pacman -S --noconfirm curl git chezmoi neovim fish eza ripgrep bat fzf delta
+
+# --- 3. Configure Shell (Fish) ---
+echo "Setting fish as the default shell..."
+chsh -s $(which fish)
+
+# --- 4. Dotfile Management (chezmoi) ---
+echo "Setting up dotfiles..."
+chezmoi init --apply aikenpil
+
+# --- 5. Configuring github ssh access ---
+echo "Now you have to configure github ssh acess following this manual 
+      https://docs.github.com/en/authentication/connecting-to-github-with-ssh"
