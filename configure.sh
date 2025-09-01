67cda869c198ae58c0ba16055084d90f152ff4db
aikenpil <78743601+Aikenpil@users.noreply.github.com>
2025-09-01 13:32:33 -0300

    changing script one last time for real now

--- configure.sh
+++ configure.sh
@@ -5,8 +5,8 @@
 
 #init keyring
 echo "initializating keyring"
-sudo pacman-key --init --noconfirm
-sudo pacman-key --populate --noconfirm
+sudo pacman-key --init
+sudo pacman-key --populate
 sudo pacman -Sy archlinux-keyring --noconfirm
 sudo pacman -Su --noconfirm
 
