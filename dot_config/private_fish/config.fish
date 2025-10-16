cab3b2571ab097260a327d1e3b46bc282784633e
aikenpil <78743601+Aikenpil@users.noreply.github.com>
2025-10-16 14:27:16 -0300

    some changes

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,9 @@ set -U fish_greeting
 if test -z "$NVIM"
     set -gx EDITOR nvim
 end
+
+alias grep 'rg -S --no-heading --line-number'
+alias ls 'eza -algh'
+alias find 'fd -H -E ".git" -E "node_modules" -L'
+alias cat 'bat --style=numbers --paging=never'
+zoxide init --cmd cd fish | source
