500389a5a8e5ab92575f230edf9587ec894289a7
aikenpil <78743601+Aikenpil@users.noreply.github.com>
2026-04-28 23:12:54 -0300

    removing nvim-treesitter

--- dot_config/nvim/lua/plugins/theme/treesitter.lua
+++ dot_config/nvim/lua/plugins/theme/treesitter.lua
@@ -1,27 +1,22 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  lazy = false,
-  config = function()
-    require("nvim-treesitter").setup({
+  {
+    "arborist-ts/arborist.nvim",
+    opts = {
+      prefer_wasm = true,
+      update_cadence = "weekly",
+      -- optionally ensure some languages are always available
       ensure_installed = {
-        "c", "cpp", "rust", "zig", "cmake", "make", "llvm", "lua", "regex", "diff",
+        "c",
+        "cpp",
+        "rust",
+        "zig",
+        "cmake",
+        "make",
+        "llvm",
+        "lua",
+        "regex",
+        "diff",
       },
-      auto_install = true,
-      sync_install = false,
-      ignore_install = {},
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          node_decremental = "grc",
-          scope_incremental = "grm",
-        },
-      },
-    })
-    vim.treesitter.language.register("bash", "zsh")
-  end,
+    },
+  },
 }
